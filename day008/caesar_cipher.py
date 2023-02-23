7f899944b972c01f82bf5e1be1736af1a9fb5973
Ragnar-B <mw.vandijk@gmail.com>
2023-02-23 17:56:20 +0100

    finish day8

--- day008/caesar_cipher.py
+++ day008/caesar_cipher.py
@@ -1,20 +1,20 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-output = []
+from art import logo
 
 def caesar(text, shift, direction):
+  output = []
   for letter in text:
-    if direction == "encode":
-      shifted = alphabet.index(letter) + shift
-    elif direction == "decode":
-      shifted = alphabet.index(letter) - shift
-    if shifted > 25:
-      shifted -= 26
-    elif shifted < 0:
-      shifted += 26
-    output.append(alphabet[shifted])
+    if letter not in alphabet:
+      output.append(letter)
+    else: 
+      if direction == "encode":
+        shifted = alphabet.index(letter) + shift
+      elif direction == "decode":
+        shifted = alphabet.index(letter) - shift
+      if shifted > 25:
+        shifted -= 26
+      elif shifted < 0:
+        shifted += 26
+      output.append(alphabet[shifted])
   if direction == "encode":
     print(f"The encoded text is {''.join(output)}")
   elif direction == "decode":
@@ -22,38 +22,15 @@ def caesar(text, shift, direction):
   else:
     print("You haven't entered encode or decode.\nPlease try again.")
      
-caesar(text, shift, direction)
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-
-
-# def encrypt(text, shift):
-#   for letter in text:
-#     shifted = alphabet.index(letter) + shift
-#     if shifted > 25:
-#       shifted -= 26
-#     output.append(alphabet[shifted])
-#   print(f"The encoded text is {''.join(output)}")
-
-# def decrypt(text, shift):
-#   for letter in text:
-#     shifted = alphabet.index(letter) - shift
-#     if shifted < 0:
-#       shifted += 26
-#     output.append(alphabet[shifted])
-#   print(f"The decoded text is {''.join(output)}")
-
-# if direction == "encode":
-#     text = input("Type your message:\n").lower()
-#     shift = int(input("Type the shift number:\n"))
-#     encrypt(text, shift)
-# elif direction == "decode":
-#     text = input("Type your message:\n").lower()
-#     shift = int(input("Type the shift number:\n"))
-#     decrypt(text, shift)
-# else:
-#    print("You haven't entered encode or decode.\nPlease try again.")
-
-# #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
-# #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+running = "yes"
+while running == "yes":
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  if shift > 26:
+    shift %=  26
+  caesar(text, shift, direction)
+  running = input("Would you like to continue? Type yes or no.")
